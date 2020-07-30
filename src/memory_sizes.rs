1409f2de96644be1c864fd30c3d8579097e7013b
caklimas <caklimas@gmail.com>
2020-07-30 12:49:15 -0400

    Added support for Mapper066

--- src/memory_sizes.rs
+++ src/memory_sizes.rs
@@ -1,7 +1,8 @@
 pub const KILOBYTES_1: u16 = 0x0400;
 pub const KILOBYTES_4: u16 = 0x1000;
 pub const KILOBYTES_8: u16 = 0x2000;
-pub const KILOBYTES_8_MASK: u16 = 0x1FFF;
+pub const KILOBYTES_8_MASK: u16 = KILOBYTES_8 - 1;
 pub const KILOBYTES_16: u16 = 0x4000;
-pub const KILOBYTES_16_MASK: u16 = 0x3FFF;
-pub const KILOBYTES_32_MASK: u16 = 0x7FFF;
+pub const KILOBYTES_16_MASK: u16 = KILOBYTES_16 - 1;
+pub const KILOBYTES_32: u16 = 0x8000;
+pub const KILOBYTES_32_MASK: u16 = KILOBYTES_32 - 1;
